54e4fc89a5d7c695eb7debabd7aae8aa13836fcc
Ronaldoss878 <98910579+Ronaldosdeltas@users.noreply.github.com>
2024-08-16 14:46:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function function1() {
+    console.log("funtions");
+    console.log(3+3);
+}
+function1();
+
+function calculateTax(cost, tax = 0.2){
+    console.log (cost* tax); 
+}
+calculateTax(200, 0.2); 
+calculateTax(3000);
